90827d68f72bcf9f7eeedc19a2662933bd29522e
Jan Buschtöns <buschtoens@gmail.com>
2012-08-12 12:17:41 +0300

    Add error listener to example

--- examples/index.js
+++ examples/index.js
@@ -2,11 +2,19 @@
 var votifier = require("votifier")(__dirname + "/private.pem");
 
 votifier.on("vote", function(user, server, ip, date) {
+	// now you could reward your user
 	console.log(
 		"Received Vote!",
 		" -> Username:   " + user,
 		" -> Serverlist: " + server,
 		" -> User IP:    " + ip,
 		" -> Date:       " + date.toGMTString()
+	);
+
+votifier.on("error", function(error) {
+	// should never happen
+	console.log(
+		"D'oh!",
+		error
 	);
 });
